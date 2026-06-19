77511ea5de4ec15747f7aabe2c7ba5527e7ca834
Arne Ludwig <arne.ludwig@posteo.de>
2026-06-19 22:41:32 +0200

    chore: add testing-library/react, jsdom env, fast-deep-equal

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,12 +7,35 @@ export default defineConfig({
         test: {
           name: 'rjsf-v6',
           include: ['tests/**/*.test.ts'],
+          environment: 'node',
+        },
+      },
+      {
+        test: {
+          name: 'rjsf-v6-react',
+          include: ['tests/**/*.test.tsx'],
+          environment: 'jsdom',
         },
       },
       {
         test: {
           name: 'rjsf-v5',
           include: ['tests/**/*.test.ts'],
+          environment: 'node',
+        },
+        resolve: {
+          alias: {
+            '@rjsf/core': 'rjsf-core-v5',
+            '@rjsf/utils': 'rjsf-utils-v5',
+            '@rjsf/validator-ajv8': 'rjsf-validator-v5',
+          },
+        },
+      },
+      {
+        test: {
+          name: 'rjsf-v5-react',
+          include: ['tests/**/*.test.tsx'],
+          environment: 'jsdom',
         },
         resolve: {
           alias: {
